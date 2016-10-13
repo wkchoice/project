ff1a5b37e609ea07e7a4ab7e646ffa943f0f82d3
ewankch <wankyu.choi@ericsson.com>
2016-10-13 18:02:22 +0900

    .

--- Day4/src/io/SerializationEx1.java
+++ Day4/src/io/SerializationEx1.java
@@ -8,11 +8,13 @@ import java.io.ObjectOutputStream;
 public class SerializationEx1 { // °´Ã¼ÀÇ serialization
 	public static void main(String[] args) throws IOException {
 		Student s1 = new Student("S001", "È«±æµ¢", 30);
+		Student s2 = new Student("S002", "¹Ú±æµ¢", 20);
 		
 		FileOutputStream fos = new FileOutputStream("Student.dat");
 		
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
 		oos.writeObject(s1);
+		oos.writeObject(s2);
 		oos.close();
 	}
 }
