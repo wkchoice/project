f00faab652f1b37fef53f792521661d2a145f4ef
wkchoice <wkchoice@DESKTOP-OMFTQJT>
2016-10-18 01:20:32 +0900

    .

--- Day5/src/networkdb/TcpDBAll.java
+++ Day5/src/networkdb/TcpDBAll.java
@@ -124,3 +124,127 @@ public class DBJob { // 5.
 		return rsMsg;
 	}
 }
+
+class Server {
+	final static int port = 9898;
+	ServerSocket ss = new ServerSocket(port);
+	Socket s = null;
+	
+	while ((s = ss.accept()) != null) {
+		Thread t = new Thread (new ServerThread(s));
+		t.setDaemon(true);
+		t.start();
+	}
+}
+
+
+// 
+FileOutputStream output = new FileOutputStream("c:/out.txt");
+for(int i=1; i<11; i++) {
+    String data = i+" 번째 줄입니다.\r\n";// 개행 문자 필수
+    output.write(data.getBytes());
+}
+output.close();
+
+FileWriter fw = new FileWriter("c:/out.txt");
+for(int i=1; i<11; i++) {
+    String data = i+" 번째 줄입니다.\r\n"; // 개행 문자 필수
+    fw.write(data);
+}
+fw.close();
+
+PrintWriter pw = new PrintWriter("c:/out.txt");
+for(int i=1; i<11; i++) {
+    String data = i+" 번째 줄입니다.";// 개행 문자 불필요
+    pw.println(data);
+}
+pw.close();
+
+// 내용 추가
+FileWriter fw = new FileWriter("c:/out.txt");
+for(int i=1; i<11; i++) {
+    String data = i+" 번째 줄입니다.\r\n";
+    fw.write(data);
+}
+fw.close();
+
+FileWriter fw2 = new FileWriter("c:/out.txt", true); // 내용추가
+for(int i=11; i<21; i++) {
+    String data = i+" 번째 줄입니다.\r\n";
+    fw2.write(data);
+}
+fw2.close();
+
+// 내용 추가
+PrintWriter pw = new PrintWriter("c:/out.txt");
+for(int i=1; i<11; i++) {
+    String data = i+" 번째 줄입니다.";
+    pw.println(data);
+}
+pw.close();
+
+
+PrintWriter pw2 = new PrintWriter(new FileWriter("c:/out.txt", true));
+for(int i=11; i<21; i++) {
+    String data = i+" 번째 줄입니다.";
+    pw2.println(data);
+}
+pw2.close();
+
+// 읽기(read)
+byte[] b = new byte[1024];
+FileInputStream input = new FileInputStream("c:/out.txt");
+input.read(b);
+System.out.println(new String(b));
+input.close();
+
+BufferedReader br = new BufferedReader(new FileReader("c:/out.txt"));
+while(true) {
+    String line = br.readLine();
+    if (line==null) break;
+    System.out.println(line);
+}
+br.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
